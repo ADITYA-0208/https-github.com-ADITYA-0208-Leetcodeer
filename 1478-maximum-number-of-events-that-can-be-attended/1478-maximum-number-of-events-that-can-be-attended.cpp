2af3274d6923d1d68caaa68973f341a1e6d5c35e
ADITYA-0208 <142163734+ADITYA-0208@users.noreply.github.com>
2025-07-18 00:41:40 +0530

    Time: 1437 ms (5%), Space: 430.8 MB (5.33%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -5,20 +5,21 @@ public:
             return a[1] < b[1];  
         return a[0] < b[0];      
     }
-    int maxEvents(vector<vector<int>>& events) {   
-        sort(events.begin(),events.end(),customComparator);
-        unordered_set<int>set1;
-        int n=events.size();
-        int maxi
-        for(int i=0;i<n;i++){
-            for(int j=events[i][0];j<=events[i][1];j++){
-                if(set1.find(j)==set1.end()){
-                    set1.insert(j);
-                    break;
-                }
+
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end(), customComparator);
+        set<int> availableDays;
+        // Insert all possible days up to max end day
+        for (int i = 1; i <= 1e5; i++) availableDays.insert(i);
+
+        int count = 0;
+        for (auto& event : events) {
+            auto it = availableDays.lower_bound(event[0]); // first available day ≥ start
+            if (it != availableDays.end() && *it <= event[1]) {
+                count++;
+                availableDays.erase(it); // assign this day
             }
         }
-        if(set1.size()>n)   return n;
-        return set1.size();
+        return count;
     }
 };
