ea5e0d2bf7be37ac905287509a818c90408c9702
ADITYA-0208 <142163734+ADITYA-0208@users.noreply.github.com>
2025-08-26 01:01:43 +0530

    Time: 67 ms (8.99%), Space: 22.6 MB (81.55%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
-        if (matrix.empty() || matrix[0].empty()) return {};
-
-        int m = matrix.size(), n = matrix[0].size();
-        vector<int> result(m * n);
-        int row = 0, col = 0;
-
-        for (int i = 0; i < m * n; i++) {
-            result[i] = matrix[row][col];
-
-            if ((row + col) % 2 == 0) {
-                if (col == n - 1) row++;
-                else if (row == 0) col++;
-                else { row--; col++; }
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<int> result;
+        int max_d = (n-1) + (m-1);
+        for (int d = 0; d <= max_d; d++) {
+            if (d % 2 == 0) {
+                for (int i = m-1; i>=0; i--) {
+                    int j = d - i;
+                    if (j >=0 && j<n) {
+                        result.push_back(mat[i][j]);
+                    }
+                }
             } else {
-                if (row == m - 1) col++;
-                else if (col == 0) row++;
-                else { row++; col--; }
+                for (int i=0;i<m;i++) {
+                    int j = d-i;
+                    if (j>=0 && j<n) {
+                        result.push_back(mat[i][j]);
+                    }
+                }
             }
         }
-
         return result;
     }
 };
