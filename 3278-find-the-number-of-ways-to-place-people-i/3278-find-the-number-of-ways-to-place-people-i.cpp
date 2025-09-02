9363f9db1d9d05dbf197a28543fb19e95c34ea8e
ADITYA-0208 <142163734+ADITYA-0208@users.noreply.github.com>
2025-09-02 15:06:46 +0530

    Time: 0 ms (100%), Space: 32.6 MB (74.81%) - LeetHub

---
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    static bool comparator(const vector<int>& p, const vector<int>& q){
+        return (p[0]==q[0])?p[1]<q[1]:p[0]>q[0];
+    }
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(),points.end(),comparator);
+        int n = points.size(), ans = 0;
+        for(int i=0; i<n-1; i++){
+            int y=INT_MAX, yi=points[i][1];
+            for(int j = i+1; j<n; j++){
+                int yj=points[j][1];
+                if (yj>=yi && y>yj){
+                    ans++;
+                    y=yj;
+                }
+            }
+        }
+        return ans;
+    }
+};
